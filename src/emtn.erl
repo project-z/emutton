2e7eed36ebbff39681a85860fc5fdcedda58fe1b
Andrew J Lenards <andrew.lenards@gmail.com>
2013-06-25 14:18:18 -0700

    Modified the erlang module to load `emtn_drv` instead of `emtn`; eunit test passes.

--- src/emtn.erl
+++ src/emtn.erl
@@ -5,6 +5,7 @@
 
 -on_load(init/0).
 
+-define(SHAREDLIB, "emtn_drv").
 -define(nif_stub, nif_stub_error(?LINE)).
 nif_stub_error(Line) ->
     erlang:nif_error({nif_not_loaded,module,?MODULE,line,Line}).
@@ -22,7 +23,7 @@ init() ->
                   Path ->
                       Path
               end,
-    erlang:load_nif(filename:join(PrivDir, ?MODULE), 0).
+    erlang:load_nif(filename:join(PrivDir, ?SHAREDLIB), 0).
 
 new() ->
     ?nif_stub.
