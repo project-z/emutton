b39cad83c1597b304def8bd084c986117fc7e31d
Matt Stump <mrevilgnome@gmail.com>
2013-07-05 22:22:30 -0700

    fixed the erl_driver.h mismatch error, the structure of the struct was a little off, coppied one from another project

--- c_src/emtn_drv.c
+++ c_src/emtn_drv.c
@@ -13,9 +13,12 @@ typedef struct {
     void *context;
 } emtn_drv_t;
 
-static ErlDrvData start(ErlDrvPort port, char *cmd)
+static ErlDrvData
+start(
+    ErlDrvPort  port,
+    char       *cmd)
 {
-    emtn_drv_t *emtn = (emtn_drv_t *)driver_alloc(sizeof(emtn_drv_t));
+    emtn_drv_t *emtn = (emtn_drv_t *) driver_alloc(sizeof(emtn_drv_t));
 
     void *context = mutton_new_context();
     check(context, "Failed to create a Mutton Context");
@@ -31,8 +34,11 @@ error:
     return NULL;
 }
 
-static void stop(ErlDrvData handle) {
-    emtn_drv_t *driver_data = (emtn_drv_t *)handle;
+static void
+stop(
+    ErlDrvData handle)
+{
+    emtn_drv_t *driver_data = (emtn_drv_t *) handle;
     check(driver_data, "Failed to cast the handle...");
     // do something to stop it and free resources...
 
@@ -40,7 +46,11 @@ error:
     log_info("well... I didn't expect that...");
 }
 
-static void outputv(ErlDrvData handle, ErlIOVec *ev) {
+static void
+outputv(
+    ErlDrvData  handle,
+    ErlIOVec    *ev)
+{
     emtn_drv_t *emtn = (emtn_drv_t *)handle;
 
     ErlDrvPort port = emtn->port;
@@ -53,31 +63,30 @@ error:
 }
 
 static ErlDrvEntry emutton_driver_entry = {
-    NULL,                               /* init */
-    start,                              /* startup */
-    stop,                               /* shutdown */
-    NULL,                               /* output */
-    NULL,                               /* ready_input */
-    NULL,                               /* ready_output */
-    "emtn_drv",                         /* name of the driver */
-    NULL,                               /* finish */
-    NULL,                               /* handle */
-    NULL,                               /* control */
-    NULL,                               /* timeout */
-    outputv,                            /* outputv */
-    NULL,                               /* ready_async */
-    NULL,                               /* flush */
-    NULL,                               /* call */
-    NULL,                               /* event */
-    ERL_DRV_EXTENDED_MARKER,            /* ERL_DRV_EXTENDED_MARKER */
-    ERL_DRV_EXTENDED_MAJOR_VERSION,     /* ERL_DRV_EXTENDED_MAJOR_VERSION */
-    ERL_DRV_EXTENDED_MAJOR_VERSION,     /* ERL_DRV_EXTENDED_MINOR_VERSION */
-    ERL_DRV_FLAG_USE_PORT_LOCKING,      /* ERL_DRV_FLAGs */
-    NULL,                               /* void *handle2, reversed for VM use */
-    NULL,                               /* F_PTR process_exit, called when a monitored process dies */
-    NULL                                /* F_PTR stop_select, called to close an event object */
+    NULL,                       /* init */
+    start,                      /* startup */
+    stop,                       /* shutdown */
+    NULL,                       /* output */
+    NULL,                       /* ready_input */
+    NULL,                       /* ready_output */
+    (char*) "emtn_drv",         /* name of the driver */
+    NULL,                       /* finish */
+    NULL,                       /* handle */
+    NULL,                       /* control */
+    NULL,                       /* timeout */
+    outputv,                    /* process */
+    NULL,                       /* ready_async */
+    NULL,                       /* flush */
+    NULL,                       /* call */
+    NULL,                       /* event */
+    ERL_DRV_EXTENDED_MARKER,    /* ERL_DRV_EXTENDED_MARKER */
+    ERL_DRV_EXTENDED_MAJOR_VERSION, /* ERL_DRV_EXTENDED_MAJOR_VERSION */
+    ERL_DRV_EXTENDED_MINOR_VERSION, /* ERL_DRV_EXTENDED_MINOR_VERSION */
+    ERL_DRV_FLAG_USE_PORT_LOCKING /* ERL_DRV_FLAGs */
 };
 
-DRIVER_INIT(emtn_drv) {
+DRIVER_INIT(
+    emtn_drv)
+{
     return &emutton_driver_entry;
 }
--- src/emtn.erl
+++ src/emtn.erl
@@ -1,8 +1,8 @@
 -module(emtn).
 
 -export([
-        start/0,
-        stop/1
+         start/0,
+         stop/1
         ]).
 
 
@@ -16,13 +16,13 @@ load_driver() ->
 
 start() ->
     case load_driver() of
-            ok ->
-                Port = open_port({spawn, 'emtn_drv'}, [binary]),
-                {ok, {emtn, Port}};
-            {error, Err} ->
-                Msg = erl_ddll:format_error(Err),
-                {error, Msg};
-            _ -> exit({error, could_not_load_driver})
+        ok ->
+            Port = open_port({spawn, 'emtn_drv'}, [binary]),
+            {ok, {emtn, Port}};
+        {error, Err} ->
+            Msg = erl_ddll:format_error(Err),
+            {error, Msg};
+        _ -> exit({error, could_not_load_driver})
     end.
 
 stop({emtn, Port}) ->
